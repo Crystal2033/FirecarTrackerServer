b7649073d499050d0920591b9a847edfe6b2ff89
yungtimofey <super.tim-stepanov@yandex.ru>
2022-07-01 18:08:43 +0300

    FR-14 made correct demo tracker controller and service

--- src/main/java/com/aqulasoft/fireman/tracker/controllers/TrackerController.java
+++ src/main/java/com/aqulasoft/fireman/tracker/controllers/TrackerController.java
@@ -1,20 +1,22 @@
 package com.aqulasoft.fireman.tracker.controllers;
 
-import com.aqulasoft.fireman.tracker.models.VehiclePositionDto;
 import com.aqulasoft.fireman.tracker.models.VehiclePositionsRequest;
-import org.springframework.stereotype.Controller;
+import com.aqulasoft.fireman.tracker.services.TrackerServices;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/api/tracker")
 public class TrackerController {
+    private final TrackerServices trackerServices;
+
+    public TrackerController(TrackerServices trackerServices) {
+        this.trackerServices = trackerServices;
+    }
+
     @PostMapping("")
-    public VehiclePositionsRequest addPositions(VehiclePositionsRequest vehiclePositionDto) {
-        return vehiclePositionDto;
+    public VehiclePositionsRequest addPositions(VehiclePositionsRequest vehiclePositionRequest) {
+        return trackerServices.addPositions(vehiclePositionRequest);
     }
 }
--- src/main/java/com/aqulasoft/fireman/tracker/services/impl/TrackerServiceImpl.java
+++ src/main/java/com/aqulasoft/fireman/tracker/services/impl/TrackerServiceImpl.java
@@ -5,21 +5,20 @@ import com.aqulasoft.fireman.tracker.controllers.TrackerController;
 import com.aqulasoft.fireman.tracker.models.VehiclePositionsRequest;
 import com.aqulasoft.fireman.tracker.services.TrackerServices;
 import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
 
+@Service
 public class TrackerServiceImpl implements TrackerServices {
     private final ModelMapper mapper;
     private final VehiclePositionCache dictionary;
-    private final TrackerController trackerController;
 
-
-    public TrackerServiceImpl(ModelMapper mapper, VehiclePositionCache dictionary, TrackerController trackerController) {
+    public TrackerServiceImpl(ModelMapper mapper, VehiclePositionCache dictionary) {
         this.mapper = mapper;
         this.dictionary = dictionary;
-        this.trackerController = trackerController;
     }
 
     @Override
     public VehiclePositionsRequest addPositions(VehiclePositionsRequest vehiclePositionsRequest) {
-        return trackerController.addPositions(vehiclePositionsRequest);
+        return vehiclePositionsRequest;
     }
 }
